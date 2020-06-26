e4b088964eadeb721b72771e9ce6752829552d84
Joel Olivero <joelolivero@outlook.com>
2020-06-25 20:32:42 -0400

    User Schema

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const User = require("/models");
 
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("connected to MongoDb.."))
   .catch((err) => console.error("Could not connect to mongodb...", err));
+
+  async function createRental {
+    
+  }
--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 const mongoose = require("mongoose");
+const app = require("");
+
+const customerSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Customer = mongoose.model("customer", customerSchema);
