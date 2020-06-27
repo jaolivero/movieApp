2f0c5ed3f3a1c600659435b1b3982b730ad6cdc8
Joel Olivero <joelolivero@outlook.com>
2020-06-26 23:59:23 -0400

    connecting to db

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const express = require("express");
 const mongoose = require("mongoose");
-const User = require("/models");
+const User = require("../movieApp/models/User");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://localhost/playground/movieApp")
   .then(() => console.log("connected to MongoDb.."))
   .catch((err) => console.error("Could not connect to mongodb...", err));
 
-  async function createRental {
-    
-  }
+async function createCustomer() {
+  const customer = new Customer({
+    firstName: "Joel",
+    lastName: "Olivero",
+    email: "joelolivero.student@careerdevs.com",
+    isGold: true,
+  });
+
+  const result = await createCustomer.save();
+  console.log(result);
+}
+
+createCustomer();
