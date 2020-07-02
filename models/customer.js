24c932a344964419a93d09475024d70dc9081da6
Joel Olivero <joelolivero@outlook.com>
2020-07-02 19:33:17 -0400

    movie app schema

--- models/customer.js
+++ models/customer.js
@@ -25,7 +25,7 @@ const Customer = mongoose.model(
 
 function validateCustomer(customer) {
   const schema = {
-    name: Joi.string().min(5).max(50).required(),
+    name: Joi.string().min(3).max(50).required(),
     phone: Joi.string().min(5).max(50).required(),
     isGold: Joi.boolean(),
   };
--- routes/movies.js
+++ routes/movies.js
@@ -14,19 +14,20 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByID(req.body.genreId);
-  if(!genre) return res.status(400)
-  res.send(genre).send('Invalid genre.');
+  if (!genre) return res.status(400);
+  res.send(genre).send("Invalid genre.");
 
   let movie = new Movie({
-      title: req.body.title,
-      genre: {
-          _id: genre._id,
-          name: genre.name
-      },
-      numberinStock: req.body.numberInStock,
-      dailyRentalRate: req.body.dailyRentalRate
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberinStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
   });
   movie = await movie.save();
+});
 
 router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
