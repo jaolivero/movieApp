34727898363bc249e365cdc2e61bce9387a72561
Joel Olivero <joelolivero@outlook.com>
2020-07-07 17:43:19 -0400

    finished code for registering user

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,20 @@ const router = express.Router();
 const { User, validate } = require("../models/genre");
 
 router.post("/", async (req, res) => {
-  const users = await Genre.find().sort("name");
-  res.send(users);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  await user.save();
+  res.send(user);
 });
 
 module.exports = router;
